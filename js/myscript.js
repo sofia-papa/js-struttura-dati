1e435951326f20395fe6c720268195e000259549
sofia-papa <papa.sofia@outlook.it>
2021-09-08 20:32:27 +0100

    description + abilities + illustrator

--- js/myscript.js
+++ js/myscript.js
@@ -13,6 +13,31 @@ let card = {
         green : 0,
     },
 
+    cardType : "creature",
+    subType : "giant warrior",
+    expansion : {
+        expansionID : 157,
+        name : "commander2015",
+        rarity : "rare",
+    },
+
+    flavourText : {
+        author : "Belko",
+        quote : "Oh certo, bevono un sacco di birra, ma dopo le loro visite devo sempre ricostruire la taverna."
+    },
+
+    abilities :
+        {
+          description : "Hammerfist Giant deals 4 damage to each creature without flying and each player."
+        },
+
+    illustrator: {
+        name : "Carl",
+        surname : "Critchlow"
+    },
+
+    cardImage : "img/pugnodiferro.png",
+
     convertedManaCost :
     function(){
         let sum = 0;
@@ -24,3 +49,25 @@ let card = {
 };
 
 console.log(card);
+
+let cardDisplayHTML =
+`<ul class= "card-element">
+      <li>Name: ${card.name}</li>
+      <li>Launch combined cost: ${card.convertedManaCost()}</li>
+      <li>Specific launch cost: ${card.launchCost}</li>
+      <li>Card type: ${card.cardType} ${card.subType}</li>
+      <li>Expansion:
+           <ul class= "expansions">
+               <li>Name: ${card.expansion.name}</li>
+               <li>Rarity: ${card.expansion.rarity}</li>
+           </ul>
+      </li>
+      <li>Flavour Text: ${card.flavourText.author} <em>${card.flavourText.quote}</em></li>
+      <li>Illustrator: ${card.illustrator.name} ${card.illustrator.surname} </li>
+      <li>Abilities: ${card.abilities.description}  </li>
+      <img src="${card.cardImage}"
+</ul>`;
+
+console.log(card.cardImage);
+
+document.getElementById("deck").innerHTML += cardDisplayHTML;
